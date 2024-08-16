0d885e635b5f39e67f4398d724a468f0af854a7c
Gabriel Lennon <gabriel.lennon@ifood.com.br>
2024-08-16 10:25:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline text-red-800">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
+
+export default App
